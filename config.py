27426f7b9c57b6616768a11cae8bc7b74a97abd4
HyDrix777 <94365103+HyDrix777@users.noreply.github.com>
2021-12-28 01:50:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ API_ID = int(os.environ.get("API_ID", "6"))
 API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
 ARQ_API_KEY = "XQYJAL-HTSZIK-YALWDS-TJPWMO-ARQ" 
-LANGUAGE = "ml"
+LANGUAGE = "en"
 ARQ_API_BASE_URL = "https://thearq.tech"
 
 bot = Client(":memory:",
