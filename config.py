b2405b54a24472d3ab4f7c7bb5b902afe68b852d
HyDrix777 <94365103+HyDrix777@users.noreply.github.com>
2022-02-21 01:11:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,10 +3,10 @@ import os
 from heroku3 import from_key
 from pyrogram import Client
 
-API_ID = int(os.environ.get("API_ID", "6"))
-API_HASH = os.environ.get("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
-ARQ_API_KEY = "XQYJAL-HTSZIK-YALWDS-TJPWMO-ARQ" 
+API_ID = int(os.environ.get("API_ID", "18891187"))
+API_HASH = os.environ.get("API_HASH", "7d120384f48b2a86fa2b9e9772a28af6")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "5179683696:AAFMoqtYwDWH2rhjELNLvuITskDX5gk4InY")
+ARQ_API_KEY = "AQNBOO-IUZKEA-XNSPAM-DWFVBF-ARQ" 
 LANGUAGE = "en"
 ARQ_API_BASE_URL = "https://thearq.tech"
 
